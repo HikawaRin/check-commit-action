7d4a8ed357ac12f1db99c2ec49a3398292cd5136
HikawaRin <releaseavalon@gmail.com>
2020-07-14 23:15:05 +0800

    feat: support pull_request check

--- index.js
+++ index.js
@@ -12,20 +12,35 @@ try {
 
   var commitMessage; 
   if (JSON.parse(payload).hasOwnProperty("pull_request")){
-    commitMessage = JSON.stringify(github.context.payload.pull_request.commits_url);
+    url = JSON.stringify(github.context.payload.pull_request.commits_url);
+    (async function(){
+      let res = await axios.get(url);
+      commitMessage = JSON.stringify(res.data[0].commit.message);
+      console.log(commitMessage);
+
+      const bashcmd = `./lint.sh ${commitMessage}`;
+      console.log(bashcmd);
+      process.execSync(bashcmd);
+      
+      const message = fs.readFileSync('error_message.txt');
+      if (message.toString() != ""){
+        throw new Error(message.toString());
+      }
+    })();
   }else{
     commitMessage = JSON.stringify(github.context.payload.commits[0].message);
+    console.log(`Commit Message is: ${commitMessage}`);
+
+    const bashcmd = `./lint.sh ${commitMessage}`;
+    console.log(bashcmd);
+    process.execSync(bashcmd);
+    
+    const message = fs.readFileSync('error_message.txt');
+    if (message.toString() != ""){
+      throw new Error(message.toString());
+    }
   }
-  console.log(`Commit Message is: ${commitMessage}`);
-  
-  const bashcmd = `./lint.sh ${commitMessage}`;
-  console.log(bashcmd);
-  process.execSync(bashcmd);
-  
-  const message = fs.readFileSync('error_message.txt');
-  if (message.toString() != ""){
-    throw new Error(message.toString());
-  }
+  // console.log(`Commit Message is: ${commitMessage}`);
 } catch (error) {
   core.setFailed(error.message);
 }
