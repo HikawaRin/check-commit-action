5f98bb98902d76500439128297fcbe67591d5de5
HikawaRin <releaseavalon@gmail.com>
2020-07-19 17:12:10 +0800

    fix: move code to try

--- index.js
+++ index.js
@@ -13,19 +13,24 @@ try {
 
   var commitMessage; 
   if (JSON.parse(payload).hasOwnProperty("pull_request")){
-    url = JSON.stringify(github.context.payload.pull_request.commits_url);
+    var url = JSON.stringify(github.context.payload.pull_request.commits_url);
+    console.log(url);
     (async function(){
-      let res = await axios.get(url);
-      commitMessage = JSON.stringify(res.data[0].commit.message);
-      console.log(commitMessage);
+      try {
+        let res = await axios.get(url);
+        commitMessage = JSON.stringify(res.data[0].commit.message);
+        console.log(commitMessage);
 
-      const bashcmd = `./lint.sh ${commitMessage}`;
-      console.log(bashcmd);
-      process.execSync(bashcmd);
-      
-      const message = fs.readFileSync('error_message.txt');
-      if (message.toString() != ""){
-        throw new Error(message.toString());
+        const bashcmd = `./lint.sh ${commitMessage}`;
+        console.log(bashcmd);
+        process.execSync(bashcmd);
+        
+        const message = fs.readFileSync('error_message.txt');
+        if (message.toString() != ""){
+          throw new Error(message.toString());
+        }
+      }catch(e){
+        console.log(e);
       }
     })();
   }else{
